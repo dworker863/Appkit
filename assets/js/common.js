c8977502eef0aff5019ae194f62db43f46d810ef
Timur Jalilov <dworker863@gmail.com>
2019-12-23 18:05:59 +0600

    Slider interactivity and animation
    * Add: autoplay for Slider
    * Add: click eventListener for indicators
    * Add: transition between slides and indicators

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,34 @@
 "use strict";
 
-$(document).ready(() => {
-  $(window).scroll(() => {
+$(document).ready(function() {
+
+  setInterval(function() {
+    if ($(".slide-active").next(".slide").length > 0) {
+      $(".slide-active").removeClass("slide-active").next(".slide").addClass("slide-active");
+      $(".active").removeClass("active").next(".indicator").addClass("active");
+    } else {
+      $(".slide-active").removeClass("slide-active");
+      $("#bg1").addClass("slide-active");
+      $(".active").removeClass("active");
+      $(".indicator").first().addClass("active");
+    }
+    
+  }, 5000);
+
+  $(window).scroll(function() {
     if ($(window).scrollTop() > 0) {
       $(".top-line").addClass("fixed");
     } else {
       $(".top-line").removeClass("fixed");
     }
   });
-})
+
+  $(".indicator").click(function(event) {
+    event.preventDefault();
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    $(".slide-active").removeClass("slide-active");
+    $($(this).attr("href")).addClass("slide-active");
+  })
+
+});
